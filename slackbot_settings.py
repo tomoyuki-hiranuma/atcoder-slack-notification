f209a1fb9a343746fe0891dccfc91274a2cdbcfc
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-10 22:58:52 +0900

    revise api key

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,8 +1,8 @@
 import os
 
 #環境変数の取得
-API_TOKEN = "xoxb-354241991779-1842862182673-SbltmwtxB8nsXip8dFBiYwrI"#os.environ["API_TOKEN"]
-DEFAULT_REPLY = "こんにちは、AtCoder通知Botです"#os.environ["DEFAULT_REPLY"]
+API_TOKEN = os.environ["API_TOKEN"]
+DEFAULT_REPLY = os.environ["DEFAULT_REPLY"]
 CHANNEL = "kyo-pro"
 
 PLUGINS = ['plugins']
